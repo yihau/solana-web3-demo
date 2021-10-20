0b24a180952926fce28f768e4a983842fbf71e8b
yihau <a122092487@gmail.com>
2021-10-20 22:16:20 +0800

    feat: add phantom wallet recover into example

--- tour/create-keypair/main.en.ts
+++ tour/create-keypair/main.en.ts
@@ -1,4 +1,5 @@
 import { Keypair } from "@solana/web3.js";
+import * as bs58 from 'bs58';
 
 // create keypair
 
@@ -12,6 +13,15 @@ async function main() {
 
   // also you can print private key
   console.log(`prikey: [${Array.from(alice.secretKey)}]`);
+
+  // =========================================================
+
+  // you can use bs58 encode to get the format of private key which phantom used
+  console.log(`private key for phantom:: ${bs58.encode(alice.secretKey)}`);
+
+  // you also can recover phantom wallet's private to web3 keypair object
+  let fromPhantom = Keypair.fromSecretKey(bs58.decode("5MaiiCavjCmn9Hs1o3eznqDEhRwxo7pXiAYez7keQUviUkauRiTMD8DrESdrNjN8zd9mTmVhRvBJeg5vhyvgrAhG"))
+  console.log(`address from phantom private: ${fromPhantom.publicKey.toBase58()}`)
 }
 
 main().then(
--- tour/create-keypair/main.ts
+++ tour/create-keypair/main.ts
@@ -1,4 +1,5 @@
 import { Keypair } from "@solana/web3.js";
+import * as bs58 from 'bs58';
 
 // 建立Solana的帳戶
 
@@ -11,6 +12,15 @@ async function main() {
 
   // 也可以印出私鑰的數值
   console.log(`prikey: [${Array.from(alice.secretKey)}]`);
+
+  // =========================================================
+
+  // 如果你想要匯入到phantom, 可以透過以下方式拿到phantom的private格式
+  console.log(`private key for phantom:: ${bs58.encode(alice.secretKey)}`);
+
+  // 同樣的，如果你有phantom的私鑰，也能反向做bs58 decode來還原成 web3 js的keypair
+  let fromPhantom = Keypair.fromSecretKey(bs58.decode("5MaiiCavjCmn9Hs1o3eznqDEhRwxo7pXiAYez7keQUviUkauRiTMD8DrESdrNjN8zd9mTmVhRvBJeg5vhyvgrAhG"))
+  console.log(`address from phantom private: ${fromPhantom.publicKey.toBase58()}`)
 }
 
 main().then(
