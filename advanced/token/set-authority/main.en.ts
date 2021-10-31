82920e73c96e091f39ccd76eb2ad77ed4247cf02
yihau <a122092487@gmail.com>
2021-10-31 13:01:32 +0800

    make describe more detail

--- advanced/token/set-authority/main.en.ts
+++ advanced/token/set-authority/main.en.ts
@@ -2,15 +2,20 @@ import { Transaction } from "@solana/web3.js";
 import { CONNECTION, TEST_MINT, FEE_PAYER } from "../../../helper/const";
 import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
 
+// you can set/update your account's auth
+// there are 4 types => 'MintTokens' | 'FreezeAccount' | 'AccountOwner' | 'CloseAccount'
+// MintTokens and FreezeAccount are for mint account
+// AccountOwner and CloseAccount are for token account
+
 async function main() {
   let tx = new Transaction();
   tx.add(
     Token.createSetAuthorityInstruction(
-      TOKEN_PROGRAM_ID, // fixed
-      TEST_MINT,
+      TOKEN_PROGRAM_ID, // always token program.
+      TEST_MINT, // mint acocunt or token account
       null, // new auth, if you want to turn off the auth, just pass null
       "MintTokens", // authority type, there are 4 types => 'MintTokens' | 'FreezeAccount' | 'AccountOwner' | 'CloseAccount'
-      FEE_PAYER.publicKey, // mint ori auth
+      FEE_PAYER.publicKey, // original auth
       []
     )
   );
