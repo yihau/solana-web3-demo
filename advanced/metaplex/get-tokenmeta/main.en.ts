e133c71218b108c3f6d9a8a776c2da30b7e32047
yihau <a122092487@gmail.com>
2021-11-07 19:15:04 +0800

    feat: add zh get token meta example

--- advanced/metaplex/get-tokenmeta/main.en.ts
+++ advanced/metaplex/get-tokenmeta/main.en.ts
@@ -1,15 +1,15 @@
 import { programs } from "@metaplex/js";
 import { Connection, PublicKey } from "@solana/web3.js";
 
-// NFT 其實就跟 SRM, USDC ... 一樣，只是一個mint，唯一不同的地方就是他的發行量是1
+// NFT is a mint. just like SRM, USDC ..., the only different is that NFT's supply is 1
 //
-// 如果你想要抓取NFT的metadata，首先你要先知道該NFT的mint地址
-// 下面我隨便抓一個NFT來當做例子
+// if we want to get NFT's metadata, first we need to know what is the mint address.
+// here I take a random DAPE as an example
 // https://explorer.solana.com/address/9MwGzSyuQRqmBHqmYwE6wbP3vzRBj4WWiYxWns3rkR7A
 //
-// tokenmeta會存在一個PDA內，他的算法是依據NFT的mint地址算出來的
-// => ['metadata', metadata_program_id, mint_id]
-// 只要知道就可以了，因為SDK都幫我們包好了
+// tokenmeta is a PDA a which derived by mint address
+// the formula is ['metadata', metadata_program_id, mint_id]
+// is it totally fine to forget it because sdk already wrapped it for us
 
 const connection = new Connection("https://api.mainnet-beta.solana.com");
 
