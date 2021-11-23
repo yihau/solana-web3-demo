ce5382a5d9a695429ea4fa211279d3dd0beca457
yihau <a122092487@gmail.com>
2021-11-23 22:42:22 +0800

    feat: get NFT current owner add parse owner code

--- advanced/token/get-NFT-current-owner/main.ts
+++ advanced/token/get-NFT-current-owner/main.ts
@@ -1,6 +1,6 @@
-import { Connection, PublicKey } from "@solana/web3.js";
+import { Connection } from "@solana/web3.js";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
-
+import * as bs58 from "bs58";
 // get NFT current owner
 
 const connection = new Connection("https://api.mainnet-beta.solana.com");
@@ -30,4 +30,7 @@ const connection = new Connection("https://api.mainnet-beta.solana.com");
   // if everything goes right, it will be an array which only contain one item.
   // the item is a token account which hold the NFT.
   console.log(accountInfos);
+
+  // you can parse its data to retrieve the real owner (SOL address)
+  console.log(`owner: ${bs58.encode(accountInfos[0].account.data.slice(32, 64))}`);
 })();
