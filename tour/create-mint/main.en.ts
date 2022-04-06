ce1e4818bc1455e19e7429f465320a8b2a2a62d0
yihau <a122092487@gmail.com>
2022-04-07 00:46:03 +0800

    feat: update create mint

--- tour/create-mint/main.en.ts
+++ tour/create-mint/main.en.ts
@@ -1,15 +1,33 @@
-import { Keypair, Transaction, SystemProgram } from "@solana/web3.js";
+import { Keypair, Transaction, SystemProgram, Connection } from "@solana/web3.js";
 
-import { CONNECTION, FEE_PAYER } from "../../helper/const";
+import {
+  createInitializeMintInstruction,
+  getMinimumBalanceForRentExemptMint,
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
 
-import * as SPLToken from "@solana/spl-token";
+import * as bs58 from "bs58";
+
+// connection
+const connection = new Connection("https://api.devnet.solana.com");
+
+// 5YNmS1R9nNSCDzb5a7mMJ1dwK9uHeAAF4CmPEwKgVWr8
+const feePayer = Keypair.fromSecretKey(
+  bs58.decode("588FU4PktJWfGfxtzpAAXywSNt74AvtroVzGfKkVN1LwRuvHwKGr851uH8czM5qm4iqLbs1kKoMKtMJG4ATR7Ld2")
+);
+
+// G2FAbFQPFa5qKXCetoFZQEvF9BVvCKbvUZvodpVidnoY
+const alice = Keypair.fromSecretKey(
+  bs58.decode("4NMwxzmYj2uvHuq8xoqhY8RXg63KSVJM1DXkpbmkUY7YQWuoyQgFnnzn6yo3CMnqZasnNPNuAT2TLwQsCaKkUddp")
+);
 
 // create mint (create your own token)
 
-// you can treat mint as ERC-20's token address in Ethereum
-// so, SRM, RAY, USDC... all of them are mint
+// you can treat a mint as a ERC-20's token address in Ethereum
+// SRM, RAY, USDC... all of them are mints
 
-async function main() {
+(async () => {
   // create a mint account
   let mint = Keypair.generate();
   console.log(`mint: ${mint.publicKey.toBase58()}`);
@@ -18,31 +36,20 @@ async function main() {
   tx.add(
     // create account
     SystemProgram.createAccount({
-      fromPubkey: FEE_PAYER.publicKey,
+      fromPubkey: feePayer.publicKey,
       newAccountPubkey: mint.publicKey,
-      space: SPLToken.MintLayout.span,
-      lamports: await SPLToken.Token.getMinBalanceRentForExemptMint(CONNECTION),
-      programId: SPLToken.TOKEN_PROGRAM_ID,
+      space: MINT_SIZE,
+      lamports: await getMinimumBalanceForRentExemptMint(connection),
+      programId: TOKEN_PROGRAM_ID,
     }),
     // init mint
-    SPLToken.Token.createInitMintInstruction(
-      SPLToken.TOKEN_PROGRAM_ID, // program id, always token program id
-      mint.publicKey, // mint account public key
+    createInitializeMintInstruction(
+      mint.publicKey, // mint pubkey
       0, // decimals
-      FEE_PAYER.publicKey, // mint authority (an auth to mint token)
+      feePayer.publicKey, // mint authority (an auth to mint token)
       null // freeze authority (we use null first, the auth can let you freeze user's token account)
     )
   );
-  tx.feePayer = FEE_PAYER.publicKey;
 
-  let txhash = await CONNECTION.sendTransaction(tx, [mint, FEE_PAYER]);
-  console.log(`txhash: ${txhash}`);
-}
-
-main().then(
-  () => process.exit(),
-  (err) => {
-    console.error(err);
-    process.exit(-1);
-  }
-);
+  console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer, mint])}`);
+})();
--- tour/create-mint/main.ts
+++ tour/create-mint/main.ts
@@ -1,48 +1,55 @@
-import { Keypair, Transaction, SystemProgram } from "@solana/web3.js";
+import { Keypair, Transaction, SystemProgram, Connection } from "@solana/web3.js";
 
-import { CONNECTION, FEE_PAYER } from "../../helper/const";
+import {
+  createInitializeMintInstruction,
+  getMinimumBalanceForRentExemptMint,
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
 
-import * as SPLToken from "@solana/spl-token";
+import * as bs58 from "bs58";
+
+// connection
+const connection = new Connection("https://api.devnet.solana.com");
+
+// 5YNmS1R9nNSCDzb5a7mMJ1dwK9uHeAAF4CmPEwKgVWr8
+const feePayer = Keypair.fromSecretKey(
+  bs58.decode("588FU4PktJWfGfxtzpAAXywSNt74AvtroVzGfKkVN1LwRuvHwKGr851uH8czM5qm4iqLbs1kKoMKtMJG4ATR7Ld2")
+);
+
+// G2FAbFQPFa5qKXCetoFZQEvF9BVvCKbvUZvodpVidnoY
+const alice = Keypair.fromSecretKey(
+  bs58.decode("4NMwxzmYj2uvHuq8xoqhY8RXg63KSVJM1DXkpbmkUY7YQWuoyQgFnnzn6yo3CMnqZasnNPNuAT2TLwQsCaKkUddp")
+);
 
 // 創建 Mint (發行自己的token)
 
 // Mint的概念就像是Ethereum上的ERC-20的地址
 // 換句話說，USDC、RAY、SRM ... 都是mint
 
-async function main() {
-  // 創建一個mint的keypair
+(async () => {
+  // create a mint account
   let mint = Keypair.generate();
   console.log(`mint: ${mint.publicKey.toBase58()}`);
 
   let tx = new Transaction();
   tx.add(
-    // 創建一個帳戶
+    // create account
     SystemProgram.createAccount({
-      fromPubkey: FEE_PAYER.publicKey,
+      fromPubkey: feePayer.publicKey,
       newAccountPubkey: mint.publicKey,
-      space: SPLToken.MintLayout.span,
-      lamports: await SPLToken.Token.getMinBalanceRentForExemptMint(CONNECTION),
-      programId: SPLToken.TOKEN_PROGRAM_ID,
+      space: MINT_SIZE,
+      lamports: await getMinimumBalanceForRentExemptMint(connection),
+      programId: TOKEN_PROGRAM_ID,
     }),
-    // 對帳戶做mint的初始化
-    SPLToken.Token.createInitMintInstruction(
-      SPLToken.TOKEN_PROGRAM_ID, // program id, 通常固定是token program id
-      mint.publicKey, // mint account public key
-      0, // decimals
-      FEE_PAYER.publicKey, // mint authority (增發幣的權限)
-      null // freeze authority (冷凍帳戶的權限，這邊我們先留null即可)
+    // init mint
+    createInitializeMintInstruction(
+      mint.publicKey, // 幣的地址
+      0, // 位數
+      feePayer.publicKey, // 增發幣的權限
+      null // 冷凍帳戶的權限 (我們這邊先留null)
     )
   );
-  tx.feePayer = FEE_PAYER.publicKey;
 
-  let txhash = await CONNECTION.sendTransaction(tx, [mint, FEE_PAYER]);
-  console.log(`txhash: ${txhash}`);
-}
-
-main().then(
-  () => process.exit(),
-  (err) => {
-    console.error(err);
-    process.exit(-1);
-  }
-);
+  console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer, mint])}`);
+})();
