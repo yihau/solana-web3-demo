938d7da54d102a67b430ed7f6de639b73df785b5
yihau <a122092487@gmail.com>
2022-04-07 00:51:51 +0800

    feat: add get mint

--- tour/get-mint/main.en.ts
+++ tour/get-mint/main.en.ts
@@ -1,29 +1,22 @@
-import { CONNECTION, TEST_MINT } from "../../helper/const";
+import { Keypair, Transaction, SystemProgram, Connection, PublicKey } from "@solana/web3.js";
 
-import * as SPLToken from "@solana/spl-token";
+import { getMint } from "@solana/spl-token";
+
+// connection
+const connection = new Connection("https://api.devnet.solana.com");
+
+const mintPubkey = new PublicKey("2GouGzZ5Z5s8FJmwPkca8Urma64WBFZ8twRCUbLQARkb");
 
 // fetch mint info
 
 // you can get mint informations by a mint address
 
-async function main() {
-  // we can use the function which lives in @solana/spl-token to fetch mint info
-  // the parameters are, (connection, mint address, token program id, fee payer)
-  // here we just want to fetch info, so we don't need to pass fee payer
-  let token = new SPLToken.Token(CONNECTION, TEST_MINT, SPLToken.TOKEN_PROGRAM_ID, null);
-  let tokenInfo = await token.getMintInfo();
-  console.log(tokenInfo);
+(async () => {
+  let mint = await getMint(connection, mintPubkey);
+  console.log(mint);
 
   // you will find that the data not include name, symbol, image...
   // because in the begin, solana don't make these data write on chain
   // if you want to fetch these info, refer to
   // https://github.com/solana-labs/token-list
-}
-
-main().then(
-  () => process.exit(),
-  (err) => {
-    console.error(err);
-    process.exit(-1);
-  }
-);
+})();
--- tour/get-mint/main.ts
+++ tour/get-mint/main.ts
@@ -1,29 +1,22 @@
-import { CONNECTION, TEST_MINT } from "../../helper/const";
+import { Keypair, Transaction, SystemProgram, Connection, PublicKey } from "@solana/web3.js";
 
-import * as SPLToken from "@solana/spl-token";
+import { getMint } from "@solana/spl-token";
+
+// connection
+const connection = new Connection("https://api.devnet.solana.com");
+
+const mintPubkey = new PublicKey("2GouGzZ5Z5s8FJmwPkca8Urma64WBFZ8twRCUbLQARkb");
 
 // 拿mint的資訊
 
 // 你可以透過mint的地址來抓取他裡面的詳細資訊
 
-async function main() {
-  // 可以透過@solana/spl-token裡面幫我們包好的function來拿取mint資訊
-  // 這邊依序會需要, connection, mint address, token program id, fee payer
-  // 但因為我們只需要拿取資料而已，所以fee payer部分我們留null即可
-  let token = new SPLToken.Token(CONNECTION, TEST_MINT, SPLToken.TOKEN_PROGRAM_ID, null);
-  let tokenInfo = await token.getMintInfo();
-  console.log(tokenInfo);
+(async () => {
+  let mint = await getMint(connection, mintPubkey);
+  console.log(mint);
 
-  // 你會發現name, symbol, image並不在回來的資訊上
+  // 你會發現name, symbol, image並不在回來的資訊上
   // 因為當初solana並沒有把這幾個欄位也規劃上鏈
   // 如果有需要抓取相關資訊可以參考
   // https://github.com/solana-labs/token-list
-}
-
-main().then(
-  () => process.exit(),
-  (err) => {
-    console.error(err);
-    process.exit(-1);
-  }
-);
+})();
